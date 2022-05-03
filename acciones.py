92712135bddda2fb2bc876df4ad6c46c417f743b
horlando <orland20161@gmail.com>
2022-05-03 09:21:37 -0500

    se añadio validaciones

--- acciones.py
+++ acciones.py
@@ -1,3 +1,6 @@
+from multiprocessing.sharedctypes import Value
+
+
 Productos = {1:'Pantalones', 2:'Camisas', 3:'Corbatas', 4:'Casacas'}
 Precios = {1:200.00, 2:120.00, 3:50.00, 4:350.00}
 Stock = {1:50, 2:45, 3:30, 4:15}
@@ -22,34 +25,77 @@ def listar():
     print("==============================")
 
 def agregar():
-    nombre=str(input("introduce el nombre del producto: "))
-    precio=round(float(input("introduce el precio del producto: ")),2)
-    stock=int(input("introduce la cantidad del producto: "))
+    while(True):
+        try:
+            nombre=str(input("introduce el nombre del producto: "))
+            break;
+        except ValueError:
+            print("el valor ingresado es erroneo")
+
+    while(True):
+        try:
+            precio=round(float(input("introduce el precio del producto: ")),2)
+            if precio>0:
+                break;
+        except ValueError:
+            print("Ingresa un valor numerico positivo")
+    while(True):
+        try:
+            stock=int(input("introduce la cantidad del producto: "))
+            if stock>0:        
+                break;
+        except ValueError:
+            print("Ingresa un valor numerico positivo")
+    
     Productos[list(Productos)[-1]+1]=nombre
     Precios[list(Precios)[-1]+1]=precio
     Stock[list(Stock)[-1]+1]=stock
 
 def eliminar():
     while(True):
-        indice=int(input("introduce el id del producto que quieres eliminar: "))
-        if (Productos.get(indice,0)==0):
-            print("Valor de id no existe vuelve a intentarlo")
-        else:
-            break
+        try:
+            indice=int(input("introduce el id del producto que quieres eliminar: "))
+            if (Productos.get(indice,0)==0):
+                print("Valor de id no existe vuelve a intentarlo")
+            else:
+                break
+        except ValueError:
+            print("el id debe ser un numero entero positivo")
     Productos.pop(indice)
     Precios.pop(indice)
     Stock.pop(indice)
 
 def actualizar():
     while(True):
-        indice=int(input("introduce el id del producto que quieres actualizar: "))
-        if (Productos.get(indice,0)==0):
-            print("Valor de id no existe vuelve a intentarlo")
-        else:
-            break
-    nombre=input("introduce el nuevo nombre del producto: ")
-    precio=round(float(input("introduce el nuevo precio del producto: ")),2)
-    stock=int(input("introduce la nueva cantidad del producto: "))
+        try:
+            indice=int(input("introduce el id del producto que quieres actualizar: "))
+            if (Productos.get(indice,0)==0):
+                print("Valor de id no existe vuelve a intentarlo")
+            else:
+                break
+        except:
+            print("el id debe ser un numero entero positivo")
+    while(True):
+        try:
+            nombre=str(input("introduce el nuevo nombre del producto: "))
+            break;
+        except ValueError:
+            print("el valor ingresado es erroneo")
+
+    while(True):
+        try:
+            precio=round(float(input("introduce el nuevo precio del producto: ")),2)
+            if precio>0:
+                break;
+        except ValueError:
+            print("Ingresa un valor numerico positivo")
+    while(True):
+        try:
+            stock=int(input("introduce la nueva cantidad del producto: "))
+            if stock>0:        
+                break;
+        except ValueError:
+            print("Ingresa un valor numerico positivo")
     Productos[indice]=nombre
     Precios[indice]=precio
     Stock[indice]=stock
